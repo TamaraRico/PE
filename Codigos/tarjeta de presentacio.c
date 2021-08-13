07aa6ff32f9d8e91dd5df6ede63e2514d7525893
TamaraRico <71352975+TamaraRico@users.noreply.github.com>
2021-08-12 21:14:48 -0700

    Add files via upload

---
+++ Codigos/tarjeta de presentacio.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+//tarjeta de presentacion
+
+int main(void){
+    printf("                ______                                                            _____                                   \n");
+    printf("               |______|  __ _   _   _ _     _       __ _   _   _ ___  __ _   _   |  _  \   _   ___    ___                 \n");
+    printf("                 |  |   / / _\\| | | |_/_ \\_/ \\ / / _\\| | | /___| / / _\\| |  |  _  /  | | /  _|  / _ \                \n");
+    printf("                 |  |   ( (_) | \\ | |  | |  | |  ( (_) | \\ | |     ( (_) | \\  | | \ \  | | | |_  | |_| |               \n");
+    printf("                 |__|    \\__/|_|  |_|  |_|  |_|   \\__/|_|  |_|      \\__/|_|   |_|  \_\ |_| \___|  \___/                \n\n");
+
+    printf("\n                  U N I V E R S I D A D  D E  B A J A  C A L I F O R N I A\n                                   ");
+    printf("\n                                   e s t u d i a n t e                                                       \n");
+    printf("\n                                   Ing. en Computacion                                                       \n");
+    printf("\n                Col. Paseo del Guaycura               Tel. 664 583 56 52                                     \n");
+    printf("\n             Pasatiempos: leer, salir a correr, estar con mi familia y amigos                                \n");
+
+
+
+
+
+
+    return 0;
+}
